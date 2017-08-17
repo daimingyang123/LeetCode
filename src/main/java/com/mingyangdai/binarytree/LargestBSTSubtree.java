30f76f75b6f8e55ef75b6ec50da928df7004931f
mingyang.dai <mingyang.dai@qunar.com>
2017-08-17 09:40:52 +0800

    home

--- src/main/java/com/mingyangdai/binarytree/LargestBSTSubtree.java
+++ src/main/java/com/mingyangdai/binarytree/LargestBSTSubtree.java
@@ -1,52 +1,50 @@
 package com.mingyangdai.binarytree;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author mingyang.dai
  * @since 2017/8/16
  */
 public class LargestBSTSubtree {
-	
-	int count = 0;
-	private HashMap<TreeNode, Integer> maxMap = new HashMap<>();
-	private int max = 0;
+	private TreeNode root;
+	private int index;
 	
 	public int largestBSTSubtree(TreeNode root) {
 		if (root == null) return 0;
-		isBST(root);
-		return max;
+		this.root = root;
+		List<Integer> list = new ArrayList<>();
+		inorder(root, list);
+		
+		int left = 0;
+		for (int i = 0; i < index; i++) {
+			if (list.get(i+1) > list.get(i)) left++;
+			else break;
+		}
+		
+		int right = 0;
+		for (int i = index+1; i < list.size(); i++) {
+			if (list.get(i) > list.get(i-1)) right++;
+			else break;
+		}
+		
+		if (left + right == list.size()-1) return list.size();
+		return Math.max(left, right);
 	}
 	
-	private boolean isBST(TreeNode root) {
-		TreeNode left = root.left;
-		TreeNode right = root.right;
-		int localCount = 1;
-		int maxVal = root.val;
-		
-		if (left != null) {
-			if ( isBST(left) && root.val > maxMap.get(left) ) {
-				count += localCount;
-			} else {
-				return false;
-			}
-		}
-		if (right != null){
-			if ( isBST(right) && root.val < maxMap.get(right) ) {
-				count += localCount;
-				maxVal = maxMap.get(right);
-			} else {
-				return false;
-			}
-		}
-		
-		count = localCount;
-		maxMap.put(root, maxVal);
-		max = Math.max(max, count);
-		return true;
+	private void inorder(TreeNode root, List<Integer> list) {
+		if (root.left != null) inorder(root.left, list);
+		list.add(root.val);
+		if (root == this.root) index = list.size()-1;
+		if (root.right != null) inorder(root.right, list);
 	}
 	
 	public static void main(String[] args) {
-	
+		int[] nums = {10,5,15,1,8,1,16};
+		TreeNode root = TreeNode.generate(nums);
+		LargestBSTSubtree largestBSTSubtree = new LargestBSTSubtree();
+		int res = largestBSTSubtree.largestBSTSubtree(root);
+		System.out.println(res);
 	}
 }
