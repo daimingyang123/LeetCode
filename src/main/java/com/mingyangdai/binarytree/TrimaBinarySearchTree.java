2ddff53d18ac6c6040436524b6e2beb77e6787fc
mingyang.dai <mingyang.dai@qunar.com>
2017-09-11 23:01:25 +0800

    home

--- src/main/java/com/mingyangdai/binarytree/TrimaBinarySearchTree.java
+++ src/main/java/com/mingyangdai/binarytree/TrimaBinarySearchTree.java
@@ -7,10 +7,23 @@ package com.mingyangdai.binarytree;
 public class TrimaBinarySearchTree {
     public TreeNode trimBST(TreeNode root, int L, int R) {
         if (root == null) return root;
-        if (root)
+        while (root != null) {
+            if (root.val < L) root = root.right;
+            else if (root.val > R) root = root.left;
+            else break;
+        }
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
     }
     
-    private void helper(TreeNode root, TreeNode cur, int L, int R) {
-        if (cur.val < L)
+    private TreeNode helper(TreeNode root, int L, int R) {
+        if (root == null) return root;
+        if (root.val < L) {
+            return root.right;
+        } else if (root.val > R) {
+            return root.left;
+        }
+        return root;
     }
 }
