cf1b518c3707f11b0b01009af291c34c9b27ec20
mingyang.dai <mingyang.dai@qunar.com>
2017-10-14 18:54:09 +0800

    home

--- src/test/java/com/mingyangdai/AppTest.java
+++ src/test/java/com/mingyangdai/AppTest.java
@@ -1,6 +1,8 @@
 package com.mingyangdai;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * Unit test for simple App.
@@ -9,6 +11,7 @@ public class AppTest {
 	public static void main(String[] args) {
 		AppTest test = new AppTest();
 		LinkedList<Object> list = new LinkedList();
+		Map map = new HashMap();
 		list.add(1);
 	}
 }
