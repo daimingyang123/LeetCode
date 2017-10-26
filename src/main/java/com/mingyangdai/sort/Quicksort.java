fbf70dfc2ebc49b043ebf9d7ad7fb693297038ab
mingyang.dai <mingyang.dai@qunar.com>
2017-10-26 16:40:41 +0800

    work

--- src/main/java/com/mingyangdai/sort/Quicksort.java
+++ src/main/java/com/mingyangdai/sort/Quicksort.java
@@ -5,17 +5,19 @@ package com.mingyangdai.sort;
  * @since 2017/10/25
  */
 public class Quicksort {
-    public void quicksort(int[] nums) {
-        int pivot = partition(nums, 0, nums.length-1);
-        partition(nums, 0, pivot);
-        partition(nums, pivot, nums.length-1);
+    
+    public static void quicksort(int[] nums, int p, int r) {
+        if (p >= r) return;
+        int pivot = partition(nums, p, r);
+        quicksort(nums, p, pivot-1);
+        quicksort(nums, pivot+1, r);
     }
     
-    private int partition(int[] nums, int p, int r) {
+    private static int partition(int[] nums, int p, int r) {
         int q = p, j = p;
         while (j < r) {
-            if (nums[j] < r) {
-                swap(nums, j, q++);
+            if (nums[j] < nums[r]) {
+                swap(nums, j++, q++);
             } else {
                 j++;
             }
@@ -24,9 +26,17 @@ public class Quicksort {
         return q;
     }
     
-    private void swap(int[] nums, int first, int second) {
+    private static void swap(int[] nums, int first, int second) {
         int temp = nums[first];
         nums[first] = nums[second];
         nums[second] = temp;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {2,1,7,3,6,4,5};
+        quicksort(nums, 0, nums.length-1);
+        for (int n : nums) {
+            System.out.print(n);
+        }
     }
 }
