895901610ac087ce31170165123c3d35ad1b520e
mingyang.dai <mingyang.dai@qunar.com>
2017-11-01 10:27:03 +0800

    work

---
+++ src/main/java/com/mingyangdai/queue/ImplementPriorityQueue.java
@@ -0,0 +1,9 @@
+package com.mingyangdai.queue;
+
+/**
+ * @author mingyang.dai
+ * @since 2017/10/30
+ */
+public class ImplementPriorityQueue {
+    
+}
--- src/test/java/com/mingyangdai/AppTest.java
+++ src/test/java/com/mingyangdai/AppTest.java
@@ -1,17 +1,13 @@
 package com.mingyangdai;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
+import java.util.Queue;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 	public static void main(String[] args) {
-		AppTest test = new AppTest();
-		LinkedList<Object> list = new LinkedList();
-		Map map = new HashMap();
-		list.add(1);
+		Queue queue = new LinkedList();
 	}
 }
