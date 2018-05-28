3c84998d68e1742eed02d44f556923e92d34cb98
mingyang.dai <mingyang.dai@qunar.com>
2018-05-28 11:13:52 +0800

    work

--- src/test/java/com/mingyangdai/AppTest.java
+++ src/test/java/com/mingyangdai/AppTest.java
@@ -1,16 +1,10 @@
 package com.mingyangdai;
 
-import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 	
 	public static void main(String[] args) {
-		Map<BigInteger, String> map = new HashMap<>();
-//		map.put(new BigInteger("192820"), "ha")
 	}
 }
