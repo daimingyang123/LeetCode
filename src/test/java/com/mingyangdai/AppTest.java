c96f44b441c83666949d1ecb94400ef23edca55a
mingyang.dai <mingyang.dai@qunar.com>
2018-07-19 16:44:18 +0800

    work

--- src/test/java/com/mingyangdai/AppTest.java
+++ src/test/java/com/mingyangdai/AppTest.java
@@ -1,6 +1,7 @@
 package com.mingyangdai;
 
 import java.util.TreeMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Unit test for simple App.
@@ -9,6 +10,6 @@ public class AppTest {
 	
 	public static void main(String[] args) {
 		TreeMap map = new TreeMap();
-		
+		new ReentrantLock().newCondition();
 	}
 }
