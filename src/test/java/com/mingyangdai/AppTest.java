09cbc0e61b39e2c7f6f99b1e3efb804d143336c7
mingyang.dai <mingyang.dai@qunar.com>
2018-11-26 20:33:38 +0800

    work

--- src/test/java/com/mingyangdai/AppTest.java
+++ src/test/java/com/mingyangdai/AppTest.java
@@ -1,17 +1,39 @@
 package com.mingyangdai;
 
+import java.util.Arrays;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-	public void test() {
-		char[] array = new char[4];
-		new String(array);
-	}
-	public static void main(String[] args) throws InterruptedException {
-		int[] array = new int[]{228,189,160,229,165,189,13,10};
-		for (int i : array) {
-			System.out.println(Integer.toBinaryString(i));
+	public boolean canPartitionKSubsets(int[] nums, int k) {
+		Arrays.sort(nums);
+		int sum = Arrays.stream(nums).sum();
+		if (sum % k != 0) return false;
+		
+		int i = 0, j = nums.length - 1;
+		int target = sum/k;
+		int cur = 0, count = 0;
+		
+		while (i <= j) {
+			if (cur < target) {
+				cur += nums[j--];
+			} else if (cur > target) {
+				cur -= nums[++j];
+				cur += nums[i++];
+			}
+			if (cur == target) {
+				count++;
+				cur = 0;
+			}
 		}
+		return count == k;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {4,3,2,3,5,2,1};
+		int k = 4;
+		boolean res = new AppTest().canPartitionKSubsets(nums, k);
+		System.out.println(res);
 	}
 }
