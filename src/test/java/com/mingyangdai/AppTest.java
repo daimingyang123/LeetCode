4c60b983a82e1dd651893fe450cbe6c8ebe6d1f2
mingyang.dai <mingyang.dai@qunar.com>
2019-03-25 15:47:24 +0800

    work

--- src/test/java/com/mingyangdai/AppTest.java
+++ src/test/java/com/mingyangdai/AppTest.java
@@ -1,39 +1,65 @@
 package com.mingyangdai;
 
-import java.util.Arrays;
+import com.google.common.collect.Lists;
+
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-	public boolean canPartitionKSubsets(int[] nums, int k) {
-		Arrays.sort(nums);
-		int sum = Arrays.stream(nums).sum();
-		if (sum % k != 0) return false;
-		
-		int i = 0, j = nums.length - 1;
-		int target = sum/k;
-		int cur = 0, count = 0;
-		
-		while (i <= j) {
-			if (cur < target) {
-				cur += nums[j--];
-			} else if (cur > target) {
-				cur -= nums[++j];
-				cur += nums[i++];
-			}
-			if (cur == target) {
-				count++;
-				cur = 0;
-			}
-		}
-		return count == k;
-	}
-	
+    
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> wordSet = new HashSet<>();
+        for (String word : wordList) {
+            wordSet.add(word);
+        }
+        if (!wordSet.contains(endWord)) return 0;
+        
+        HashSet<String> queue = new HashSet<>();
+        queue.add(beginWord);
+        
+        int dis = 2;
+        while (!queue.isEmpty()) {
+            HashSet<String> queue2 = new HashSet<>();
+            for (String word : queue) {
+                HashSet<String> set = getSet(word, wordSet);
+                if (set.contains(endWord)) return dis;
+                queue2.addAll(set);
+                dis++;
+            }
+            queue = queue2;
+        }
+        
+        return 0;
+    }
+    
+    private HashSet<String> getSet(String word, HashSet<String> wordSet) {
+        HashSet<String> set = new HashSet<>();
+        char[] array = word.toCharArray();
+        
+        for (int i = 0; i < array.length; i++) {
+            char c = array[i];
+            for (char j = 'a'; j <= 'z'; j++) {
+                if (j == c) continue;
+                array[i] = j;
+                String s = new String(array);
+                if (wordSet.remove(s)) {
+                    set.add(s);
+                }
+            }
+            array[i] = c;
+        }
+        
+        return set;
+    }
+    
 	public static void main(String[] args) {
-		int[] nums = {4,3,2,3,5,2,1};
-		int k = 4;
-		boolean res = new AppTest().canPartitionKSubsets(nums, k);
-		System.out.println(res);
+        String beginWord = "hit";
+        String end = "cog";
+        List<String> list = Lists.newArrayList("hot", "dot", "dog", "lot", "log", "cog");
+        int dis = new AppTest().ladderLength(beginWord, end, list);
+        System.out.println(dis);
 	}
 }
